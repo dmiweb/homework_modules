43533df82179c1624e210c87551a1620ceb06d9d
dmiweb <dimbons@mail.ru>
2024-03-17 23:01:24 +0300

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,16 @@
+import dafaultCharacter from './domain.js';
+
+export default class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
